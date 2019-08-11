8a13c097eff6428a6530dd661914afb70645da3c
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2019-08-11 12:52:35 +0200

    item is being added to the card, new card is being created if it does not exist

--- UICtrl.js
+++ UICtrl.js
@@ -6,16 +6,85 @@ const UIController = (function() {
     itemNameField: "#item-name",
     itemDateField: "#item-date",
     addBtn: ".add-btn",
+    cardsContainer: ".cards-container",
+    card: ".date-card",
+    itemList: ".item-list"
 
   }
   const $q = document.querySelector.bind(document);
+  const $qa = document.querySelectorAll.bind(document);
+
+  const convertDateToCardId = function(date) {
+    const re = /[A-Za-z0-9]/;
+    const idArray = date.split('').filter(char =>  re.test(char));
+    const idToReturn = 'card-' + idArray.join('').toUpperCase();
+    return idToReturn.toString();
+
+  };
+
+  const createCard = function(item) {
+    cardHtml = `
+    <div class="col s12 m6 date-card" id="${convertDateToCardId(item.date)}">
+    <div class="card blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">${item.date}</span>
+        <ul class="collection black-text item-list" >
+        </ul>
+      </div>
+    </div>
+  </div>
+    `;
+    $q(UISelectors.cardsContainer).insertAdjacentHTML('beforeend', cardHtml);
+
+  };
+  
+  const getAllDateCards = function() {
+    return Array.from($qa(UISelectors.card))
+  };
+
+  const createListItem = function(item) {
+    const li = document.createElement('li');
+    li.className = "collection-item";
+    li.id = `item-${item.id}`;
+    
+    li.innerHTML = `
+      <strong>${item.name}: </strong> <br> 
+      <em>${item.calories} kcal</em>
+      <a href="#" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+      </a>
+    `;
+
+    return li;
+
+
+  }
+
+  const assignItemToExistingCard = function(item) {
+
+    const allCards = getAllDateCards();
+    const existingCardID = '#' + convertDateToCardId(item.date);
+    const cardToAddItem = $q(existingCardID);
+
+    const elementToAppend = createListItem(item);
+    cardToAddItem.querySelector(UISelectors.itemList).appendChild(elementToAppend);
+
+    //document.querySelector("#post").querySelectorAll('.thumb')
+
+    //allcards.forEach(function)
+
+  
+   
+
+  }
 
   return {
     initializeDatePicker: function() {
         const datepicker = $q(UISelectors.datepicker);
         M.Datepicker.init(datepicker, {
               autoClose: true,
-              format: "dd/mm/yyyy"
+              format: "dd/mm/yyyy",
+              defaultDate: new Date()
             });
     },
 
@@ -44,9 +113,19 @@ const UIController = (function() {
         calories: itemCalories
       }
     },
-    addItemToTheUI: function(item) {
-      console.log(item);
-      console.log('Now its the time to add the item to UI baby')
+    addItemToTheUI: function(item) {  
+
+      const allcards = getAllDateCards();
+      const cardIds = allcards.map(card=>card.id);
+
+      //check if the card with this ID already exist
+      if(!cardIds.includes(convertDateToCardId(item.date))) {
+        createCard(item);
+      }
+
+      assignItemToExistingCard(item);
+
+     
     }
 
    
